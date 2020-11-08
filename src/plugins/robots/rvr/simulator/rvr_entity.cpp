ca463b93a9c3f4266ef2fd0a442885105c065df6
rafftod <ramaluto@gmail.com>
2020-11-08 18:16:26 +0100

    Basic Sphero RVR entity integration

---
+++ src/plugins/robots/rvr/simulator/rvr_entity.cpp
@@ -0,0 +1,89 @@
+/**
+ * @file <argos3/plugins/robots/rvr/simulator/rvr_entity.cpp>
+ * @author Raffaele Todesco - <rtodesco@ulb.be>
+ */
+
+#include "rvr_entity.h"
+
+namespace argos {
+    const Real CRVREntity::BODY_HEIGHT = 0.114f;
+    const Real CRVREntity::BODY_LENGTH = 0.1839f;
+    const Real CRVREntity::BODY_WIDTH = 0.2159f;
+
+    CRVREntity::CRVREntity() :
+        CComposableEntity(NULL),
+        m_pcControllableEntity(NULL),
+        m_pcEmbodiedEntity(NULL) {}
+
+    CRVREntity::CRVREntity(const std::string& str_id,
+        const std::string& str_controller_id,
+        const CVector3& c_position,
+        const CQuaternion& c_orientation) :
+
+        CComposableEntity(NULL, str_id),
+        m_pcControllableEntity(NULL),
+        m_pcEmbodiedEntity(NULL) {
+
+        try {
+            /* Create and init components */
+            m_pcEmbodiedEntity = new CEmbodiedEntity(this, "body_0", c_position, c_orientation);
+            AddComponent(*m_pcEmbodiedEntity);
+
+            m_pcControllableEntity = new CControllableEntity(this, "controller_0");
+            AddComponent(*m_pcControllableEntity);
+            m_pcControllableEntity->SetController(str_controller_id);
+            UpdateComponents();
+        }
+        catch (CARGoSException& ex) {
+            LOG << ex.what() << "\n";
+            THROW_ARGOSEXCEPTION_NESTED("Failed to initialize entity \"" << GetId() << "\".", ex);
+        }
+    }
+
+    void CRVREntity::Init(TConfigurationNode& t_tree) {
+        try {
+            CComposableEntity::Init(t_tree);
+            m_pcEmbodiedEntity = new CEmbodiedEntity(this);
+            AddComponent(*m_pcEmbodiedEntity);
+            m_pcEmbodiedEntity->Init(GetNode(t_tree, "body"));
+            m_pcControllableEntity = new CControllableEntity(this);
+            AddComponent(*m_pcControllableEntity);
+            m_pcControllableEntity->Init(GetNode(t_tree, "controller"));
+            UpdateComponents();
+        }
+        catch (CARGoSException& ex) {
+            LOG << ex.what() << '\n';
+            THROW_ARGOSEXCEPTION_NESTED("Failed to initialize entity \"" << GetId() << "\".", ex);
+        }
+
+
+    }
+
+    void CRVREntity::Reset() {
+        /* Reset all components */
+        CComposableEntity::Reset();
+        /* Update components */
+        UpdateComponents();
+    }
+
+    void CRVREntity::Destroy() {
+        CComposableEntity::Destroy();
+    }
+
+#define UPDATE(COMPONENT) if(COMPONENT->IsEnabled()) COMPONENT->Update();
+
+    void CRVREntity::UpdateComponents() {
+        // no components to update for now
+    }
+
+    REGISTER_ENTITY(CRVREntity,
+        "rvr",
+        "Raffaele Todesco [rtodesco@ulb.be]",
+        "1.0",
+        "The Sphero RVR robot.",
+        "Sphero Description",
+        "Under development"
+    );
+
+    REGISTER_STANDARD_SPACE_OPERATIONS_ON_COMPOSABLE(CRVREntity);
+}
---
+++ src/plugins/robots/rvr/simulator/rvr_entity.h
@@ -0,0 +1,55 @@
+/**
+ * @file <argos3/plugins/robots/rvr/simulator/rvr_entity.h>
+ * @author Raffaele Todesco - <rtodesco@ulb.be>
+ */
+
+#ifndef RVR_ENTITY_H
+#define RVR_ENTITY_H
+
+namespace argos {
+    class CRVREntity;
+    class CEmbodiedEntity;
+    class CControllableEntity;
+}
+
+#include <argos3/core/simulator/entity/composable_entity.h>
+
+namespace argos {
+    class CRVREntity : public CComposableEntity {
+    public:
+        ENABLE_VTABLE();
+    public:
+        CRVREntity();
+
+        CRVREntity(const std::string& str_id,
+            const std::string& str_controller_id,
+            const CVector3& c_position = CVector3(),
+            const CQuaternion& c_orientation = CQuaternion());
+
+        virtual void Init(TConfigurationNode& t_tree);
+        virtual void Reset();
+        virtual void Destroy();
+        virtual void UpdateComponents();
+
+        inline CControllableEntity& GetControllableEntity() {
+            return *m_pcControllableEntity;
+        }
+
+        inline CEmbodiedEntity& GetEmbodiedEntity() {
+            return *m_pcEmbodiedEntity;
+        }
+
+        virtual std::string GetTypeDescription() const {
+            return "rvr";
+        }
+    private:
+        CControllableEntity* m_pcControllableEntity;
+        CEmbodiedEntity* m_pcEmbodiedEntity;
+    public:
+        static const Real BODY_LENGTH;
+        static const Real BODY_HEIGHT;
+        static const Real BODY_WIDTH;
+    };
+}
+
+#endif
